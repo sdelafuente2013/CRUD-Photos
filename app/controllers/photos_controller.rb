ccdd3b34835afd71c14215c32c37d2fba307a1e6
santi <santiagodelafuente2013@gmail.com>
2022-06-11 03:12:46 +0200

    add edit and destroy

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,4 +3,21 @@ class PhotosController < ApplicationController
     @photos = Photo.all
     @photo = Photo.new()
   end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.save
+
+    redirect_to photos_path
+  end
+
+  # def destroy
+  #   redirect_to photos_path
+  # end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :picture)
+  end
 end
